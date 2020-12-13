bbcec4d6d49a681decd856b8b19f074ced97ef18
Hue Pham <huepham113@gmail.com>
2020-12-12 23:25:51 -0500

    first commit

---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const HomePage = () => {
+    return (
+        <div className="homepage">  
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
