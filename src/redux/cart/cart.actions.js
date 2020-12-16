33a16db72d1ba49ccba2783afc1fc2a198ea4dd0
Hue Pham <huepham113@gmail.com>
2020-12-15 23:29:27 -0500

    Add clearItemFromCart method to cart.actions in redux

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,10 +2,15 @@ import { CartActionTypes } from './cart.types'
 
 export const toggleCartHidden = (state) => ({
     type: CartActionTypes.TOOGLE_CART_HIDDEN,
-    // payload: state          // no need to write this, because you just need the action type, then react will automatically set the opposite for this case
+    // payload: state         // no need to write this, because you just need the action type, then react will automatically set the opposite for this case
 })
 
 export const addItem = (item) => ({
     type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
+
+export const clearItemFromCart = (item) => ({ 
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
 })
