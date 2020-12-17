ee8d1e3319b5eac5c9299f442b0a4c3bbac9f167
Hue Pham <huepham113@gmail.com>
2020-12-17 16:54:34 -0500

    Added CollectionPage; Added selectCollection in redux shop; Implemented collection selector in CollectionPage

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,27 @@
 import { createSelector } from 'reselect'
 
+const COLLECTION_ID_MAP = {         // map string to number (because url :collectionId is number, but we want string)
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    women: 4,
+    mensss: 5
+}
+
 const selectShop = state => state.shop
 
 export const selectShopCollections = createSelector(
     [selectShop],
     shop => shop.collections
+)
+
+// export const selectCollection = (collectionUrlParam) => 
+//     createSelector([selectShopCollections], collections => 
+//         collections.find(collection => 
+//             collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+// )
+
+export const selectCollection = collectionUrlParam => createSelector(
+    [selectShopCollections], 
+    collections => collections.find(collection => collection.routeName === collectionUrlParam)
 )
