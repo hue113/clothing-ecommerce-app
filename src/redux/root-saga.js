ee2655f25f340cb19fcda698badf55a64ddc2cda
Hue Pham <huepham113@gmail.com>
2020-12-23 23:48:13 -0500

    Added shopSagas method and import into root-saga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,14 +1,15 @@
 import { all, call } from 'redux-saga/effects'
 
-import { fetchCollectionsStart } from './shop/shop.sagas'
+import { fetchCollectionsStart, shopSagas } from './shop/shop.sagas'
 import { userSagas } from './user/user.sagas'
 import { cartSagas } from './cart/cart.sagas'
 
 export default function* rootSaga() {
     // all() takes an array of sagas; put all sagas in this array
     yield all([                 
-        call(fetchCollectionsStart),     
+        // call(fetchCollectionsStart),     
         call(userSagas),
-        call(cartSagas)
+        call(cartSagas),
+        call(shopSagas)
     ])      
 }
